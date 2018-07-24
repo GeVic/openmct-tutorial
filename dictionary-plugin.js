974d872cf115bd91046affc8dda2e44b5b245af8
gevic <veeki.yadav@bennett.edu.in>
2018-07-24 22:32:56 +0530

    Object Provider

--- dictionary-plugin.js
+++ dictionary-plugin.js
@@ -1,3 +1,25 @@
+function getDictionary() {
+  return http.get('/dictionary.json').then(function (result){
+    return result.data;
+  });
+}
+
+var objectProvider = {
+  get: function (identifier) {
+    return getDictionary().then(function (dictionary){
+      // check for th identifier key
+      if (identifier.key == 'spacecraft') {
+        return {
+          identifier: identifier,
+          name: dictionary.name,
+          type: 'folder',
+          location: 'ROOT'
+        };
+      }
+    });
+  }
+};
+
 function DictionaryPlugin(){
   return function install() {
     // Root node using Open MCT API
@@ -5,5 +27,8 @@ function DictionaryPlugin(){
       namespace: 'space.taxonomy',
       key: 'spacecraft'
     });
+
+    // Object provider :- It will provide openmct with the object for the identifier 
+    openmct.objects.addProvider('space.taxonomy', objectProvider);
   }
 };
