84bd150b2f988767e79cf038673f6d9f4b7b6cfc
gevic <veeki.yadav@bennett.edu.in>
2018-07-24 23:24:39 +0530

    new open mct type created

--- dictionary-plugin.js
+++ dictionary-plugin.js
@@ -1,3 +1,10 @@
+// Adding types similar to 'folder'
+openmct.types.addType('example.telemetry', {
+  name: 'Example Telemetry Point',
+  description: 'For tutorial',
+  cssClass: 'icon-telemetry'
+});
+
 function getDictionary() {
   return http.get('/dictionary.json').then(function (result){
     return result.data;
@@ -15,6 +22,19 @@ var objectProvider = {
           type: 'folder',
           location: 'ROOT'
         };
+      } else {
+        var measurement = dictionary.measurements.filter(function (m) {
+          return m.key === identifier.key;
+        })[0];
+        return {
+          identifier: identifier,
+          name: measurement.name,
+          type: 'example.telemetry',
+          telemetry: {
+            values: measurement.values
+          },
+          location: 'example.taxonomy:spacecraft'
+        }
       }
     });
   }
