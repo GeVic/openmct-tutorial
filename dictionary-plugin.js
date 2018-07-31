c54b7d6eea0b5392468f70d11e540692e053cd10
gevic <veeki.yadav@bennett.edu.in>
2018-07-31 23:13:55 +0530

    tree node populated

--- dictionary-plugin.js
+++ dictionary-plugin.js
@@ -1,54 +1,72 @@
-// Adding types similar to 'folder'
-openmct.types.addType('example.telemetry', {
-  name: 'Example Telemetry Point',
-  description: 'For tutorial',
-  cssClass: 'icon-telemetry'
-});
-
 function getDictionary() {
-  return http.get('/dictionary.json').then(function (result){
-    return result.data;
-  });
+  return http.get('/dictionary.json')
+      .then(function (result) {
+          return result.data;
+      });
 }
 
 var objectProvider = {
   get: function (identifier) {
-    return getDictionary().then(function (dictionary){
-      // check for th identifier key
-      if (identifier.key == 'spacecraft') {
-        return {
-          identifier: identifier,
-          name: dictionary.name,
-          type: 'folder',
-          location: 'ROOT'
-        };
-      } else {
-        var measurement = dictionary.measurements.filter(function (m) {
-          return m.key === identifier.key;
-        })[0];
-        return {
-          identifier: identifier,
-          name: measurement.name,
-          type: 'example.telemetry',
-          telemetry: {
-            values: measurement.values
-          },
-          location: 'example.taxonomy:spacecraft'
-        }
-      }
-    });
+      return getDictionary().then(function (dictionary) {
+          if (identifier.key === 'spacecraft') {
+              return {
+                  identifier: identifier,
+                  name: dictionary.name,
+                  type: 'folder',
+                  location: 'ROOT'
+              };
+          } else {
+              var measurement = dictionary.measurements.filter(function (m) {
+                  return m.key === identifier.key;
+              })[0];
+              return {
+                  identifier: identifier,
+                  name: measurement.name,
+                  type: 'example.telemetry',
+                  telemetry: {
+                      values: measurement.values
+                  },
+                  location: 'example.taxonomy:spacecraft'
+              };
+          }
+      });
   }
 };
 
-function DictionaryPlugin(){
-  return function install() {
-    // Root node using Open MCT API
-    openmct.objects.addRoot({
-      namespace: 'space.taxonomy',
-      key: 'spacecraft'
-    });
-
-    // Object provider :- It will provide openmct with the object for the identifier 
-    openmct.objects.addProvider('space.taxonomy', objectProvider);
+var compositionProvider = {
+  appliesTo: function (domainObject) {
+      return domainObject.identifier.namespace === 'example.taxonomy' &&
+             domainObject.type === 'folder';
+  },
+  load: function (domainObject) {
+      return getDictionary()
+          .then(function (dictionary) {
+              return dictionary.measurements.map(function (m) {
+                  return {
+                      namespace: 'example.taxonomy',
+                      key: m.key
+                  };
+              });
+          });
   }
+};
+
+function DictionaryPlugin() {
+  return function install(openmct) {
+    // Defininig root node
+      openmct.objects.addRoot({
+          namespace: 'example.taxonomy',
+          key: 'spacecraft'
+      });
+      // It provides objects to the identifier (Until now it was defined)
+      openmct.objects.addProvider('example.taxonomy', objectProvider);
+      // It polpulates the rood node with telemetry measurements
+      openmct.composition.addProvider(compositionProvider);
+  
+      openmct.types.addType('example.telemetry', {
+          name: 'Example Telemetry Point',
+          description: 'Example telemetry point from our happy tutorial.',
+          cssClass: 'icon-telemetry'
+      });
+  };
 };
